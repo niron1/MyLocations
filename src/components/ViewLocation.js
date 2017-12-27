888520b26f8c1cdf4758da1d8d36f52cb636ea71
niron1 <niro@edev.co>
2017-12-27 10:33:28 +0200

    added full location view page

--- src/components/ViewLocation.js
+++ src/components/ViewLocation.js
@@ -11,12 +11,25 @@ export class ViewLocation extends React.Component {
     super(props);
     const viewId = this.props.match.params.locationId;
     let name = '';
-    if (viewId) name = this.props.locations.list[viewId].name;
-    else this.props.history.goBack();
+    let address = '';
+    let lat = '';
+    let lon = '';
+    let categoryId = '';
+    if (viewId) {
+      name = this.props.locations.list[viewId].name;
+      address = this.props.locations.list[viewId].address;
+      lat = this.props.locations.list[viewId].lat;
+      lon = this.props.locations.list[viewId].lon;
+      categoryId = this.props.locations.list[viewId].category_id;
+    } else this.props.history.goBack();
 
     this.state = {
       viewId,
       name,
+      address,
+      lat,
+      lon,
+      categoryId,
     };
   }
   render() {
@@ -34,9 +47,27 @@ export class ViewLocation extends React.Component {
                 View Location
               </h3>
             </div>
-            <div className={styles.fieldRow}>
-              <label className={styles.fieldName}>name:</label>
-              <label className={styles.fieldValue}>{this.state.name}</label>
+            <div className={styles.itemBox}>
+              <div className={styles.fieldRow}>
+                <label className={styles.fieldName}>name:</label>
+                <label className={styles.fieldValue}>{this.state.name}</label>
+              </div>
+              <div className={styles.fieldRow}>
+                <label className={styles.fieldName}>address:</label>
+                <label className={styles.fieldValue}>{this.state.address}</label>
+              </div>
+              <div className={styles.fieldRow}>
+                <label className={styles.fieldName}>lat:</label>
+                <label className={styles.fieldValue}>{this.state.lat}</label>
+              </div>
+              <div className={styles.fieldRow}>
+                <label className={styles.fieldName}>lon:</label>
+                <label className={styles.fieldValue}>{this.state.lon}</label>
+              </div>
+              <div className={styles.fieldRow}>
+                <label className={styles.fieldName}>category:</label>
+                <label className={styles.fieldValue}>{this.props.categories.list[this.state.categoryId].name}</label>
+              </div>
             </div>
             <BottomNavbar />
           </Col>
